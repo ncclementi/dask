ad5298f2a4dfeaeb4ff52a9cbdc9cdfed84c30b7
ncclementi <natyclementi@gmail.com>
2022-02-08 13:30:22 -0500

    pin scipy [test-upstream]

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -10,14 +10,13 @@ if [[ ${UPSTREAM_DEV} ]]; then
     mamba install -y -c arrow-nightlies "pyarrow>5.0"
 
     # FIXME https://github.com/mamba-org/mamba/issues/412
-    # mamba uninstall --force numpy pandas scipy fastparquet
-    conda uninstall --force numpy pandas scipy fastparquet
+    # mamba uninstall --force numpy pandas fastparquet
+    conda uninstall --force numpy pandas fastparquet
 
     python -m pip install --no-deps --pre \
         -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
         numpy \
-        pandas \
-        scipy
+        pandas 
 
     python -m pip install \
         --upgrade \
